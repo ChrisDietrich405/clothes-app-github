b2e358937ea7e0bd1b44ade19384d48b3fdbefb3
Chris <chrisdietrich3666@gmail.com>
2022-05-29 19:00:10 -0500

    added styling to cart

--- src/components/Cart/index.js
+++ src/components/Cart/index.js
@@ -1,10 +1,10 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
+import styles from "./styles.module.css";
+
 const Cart = () => {
   const [cartProducts, setCartProducts] = useState([]);
-
-  useEffect(() => {}, [cartProducts]);
 
   const navigate = useNavigate();
 
@@ -24,28 +24,45 @@ const Cart = () => {
   useEffect(() => {
     const localStorageProducts = localStorage.getItem("cartProducts");
     const products = JSON.parse(localStorageProducts);
-    setCartProducts(products);
+
+    const productsArray = [];
+    products.forEach((cartProduct) => {
+      const index = productsArray.findIndex(
+        (arrayProduct) => arrayProduct.id === cartProduct.id
+      );
+      if (index !== -1) {
+        productsArray[index].total++;
+      } else {
+        const newProduct = { ...cartProduct, total: 1 };
+        productsArray.push(newProduct);
+      }
+    });
+    setCartProducts(productsArray);
   }, []);
 
   return (
-    <>
-      <ul>
+    <div className={styles.cart_container}>
+      <ul className={styles.list_container}>
         {cartProducts.map((product) => {
           return (
-            <div>
-              <h1>{product.title}</h1>
+            <div className={styles.product_container}>
               <img src={product.image} alt={product.title} />
-              <p>{product.price}</p>
-              <button onClick={() => navigate("/products")}>
-                Back to Products
-              </button>
+              <div className={styles.product_information_container}>
+                <h4>{product.title}</h4>
+                <strong>${product.price}</strong>
+                <p>Total: {product.total}</p>
+              </div>
             </div>
           );
         })}
       </ul>
-      <div>{cartProductTotals.totalItems}</div>
-      <div>${cartProductTotals.totalPrice}</div>
-    </>
+      <div className={styles.cart_summary_container}>
+        <h3>Cart Summary</h3>
+        <p>Total: {cartProductTotals.totalItems}</p>
+        <p>Total Price: ${cartProductTotals.totalPrice}</p>
+        <button onClick={() => navigate("/products")}>Back to Products</button>
+      </div>
+    </div>
   );
 };
 
