34de9be11f6086badfa7c9159a5e57a9505ae6ee
Chris <chrisdietrich3666@gmail.com>
2024-07-10 17:00:50 -0400

    added cart route

--- server/src/controllers/cartController.js
+++ server/src/controllers/cartController.js
@@ -1,17 +1,19 @@
 import * as CartRepos from "../repositories/cartRepo.js";
 
 const getCarts = async (req, res) => {
-  console.log("shoe");
-  // try {
-  //   const carts = await CartRepos.getAll();
-  //   res.status(200).json({ carts });
-  // } catch (error) {
-  //   if (error.message === "Unable to connect to the database") {
-  //     return res.status(500).json("Problems getting data");
-  //   }
-  // }
-
-  // console.log(carts);
+  try {
+    const carts = await CartRepos.getAll();
+    res.status(200).json({ carts });
+  } catch (error) {
+    if (error.message === "Unable to connect to the database") {
+      return res.status(500).json("Problems getting data");
+    }
+  }
 };
 
-export { getCarts };
+const getUserCart = async (req, res) => {
+  const userId = req.user.id;
+  console.log(userId);
+};
+
+export { getCarts, getUserCart };
--- server/src/routes/cartRoute.js
+++ server/src/routes/cartRoute.js
@@ -1,9 +1,12 @@
 import express from "express";
 import * as CartController from "../controllers/cartController.js";
+import authMiddleware from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
 router.get("/", CartController.getCarts);
+
+router.get("/:id", authMiddleware, CartController.getUserCart);
 
 // router.get("/:id", ProductsController.getOne);
 
